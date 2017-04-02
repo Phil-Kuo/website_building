79f5b44cdf457e0df56975b24da65c2df801a1d0
Yimin Guo <yeemin.kuo@gmail.com>
2017-04-02 20:56:36 +0800

    Modify the Index Template.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render
 from rango.models import Category
 
 def index(request):
-    category_list = Category.objects.order_by('-likes')[:-5]
+    category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories':category_list}
 
     return render(request, 'rango/index.html', context_dict)
